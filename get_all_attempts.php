bf4273c2df2bd508bc6bc2f53637d82127c1f449
mdhaini2 <mohamad.dhaini02@lau.edu>
2023-05-02 00:24:41 +0300

    'debug'

--- get_all_attempts.php
+++ get_all_attempts.php
@@ -4,7 +4,7 @@ $attempt = $_GET['attempt'];
 $lift = $_GET['lift'];
 
 
-$query = $table->findSql("SELECT * FROM attempts where attempt = $attempt and lift = $lift");
+$query = $table->findSql("SELECT * FROM attempts where attempt = $attempt and lift = '$lift'");
 
 return $query;
 
